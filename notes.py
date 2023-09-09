d5f932e60a5d7178971055b156e876e0b7feaf80
Maxim <Max@example.com>
2023-09-09 22:40:36 +0300

    Создал метод по добавлению заметки и записи ее в файл

--- notes.py
+++ notes.py
@@ -12,3 +12,28 @@ def load_notes():
     return notes
 
 
+
+def save_notes(notes):
+    with open('notes.json', 'w') as f:
+        json.dump(notes, f)
+
+
+
+def add_note():
+ notes = load_notes()
+ note_id = input("Введите идентификатор заметки: ")
+ title = input("Введите заголовок заметки: ")
+ body = input("Введите текст заметки: ")
+ timestamp = input("Введите дату/время создания заметки: ")
+
+ note = {
+ 'title': title,
+ 'body': body,
+ 'timestamp': timestamp
+ }
+
+ notes[note_id] = note
+ save_notes(notes)
+ print("Заметка добавлена")
+
+
